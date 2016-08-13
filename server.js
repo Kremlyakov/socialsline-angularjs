1334206d183d70877eb0d3fe327225df1ffef08c
Eugene Boldyrev <boldyrev.gene@gmail.com>
2016-08-14 00:37:34 +0300

    add index.html and server.js files

---
+++ server.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.static('src'));
+
+app.listen(3000);
